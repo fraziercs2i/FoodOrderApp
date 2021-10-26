bf3be726dbe01888d56a86708219feb4ad752230
fraziercs2i <jgwenet@hotmail.com>
2021-10-26 22:57:19 +0200

    Initial commit

---
+++ components/Meals/MealItem/MealItem.js
@@ -0,0 +1,19 @@
+import MealItemForm from './MealItemForm';
+import classes from './MealItem.module.css';
+
+const MealItem = props => {
+
+    const price = `$${props.price.toFixed(2)}`;   //First $ to output the caracter dollar sign and one 
+                                                    //in combination with the curly braces
+    return <li classes={classes.meal}>
+            <div><h3>{props.name}</h3>
+            <div className={classes.description}>{props.description}</div>
+            <div className = {classes.price}>{price}</div>
+            </div>
+            <div>
+                <MealItemForm id = {props.id}/>
+            </div>        
+        </li>
+}
+
+export default MealItem;

4a78b87fc438c9dee19e851bbfa55ef1dbe1ff69
fraziercs2i <jgwenet@hotmail.com>
2021-10-30 08:49:05 +0200

    Feat: update the MealItemForm with useContext
    -> Use Refs and Forwards Refs
    -> Use Context

--- components/Meals/MealItem/MealItem.js
+++ components/Meals/MealItem/MealItem.js
@@ -1,17 +1,29 @@
+import { useContext } from 'react';
+
 import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
-
+import CartContext from '../../../store/cart-context';
 const MealItem = props => {
 
+    const cartCtx = useContext(CartContext);
+    const addToCartHandler = amount => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        });
+    };
+
     const price = `$${props.price.toFixed(2)}`;   //First $ to output the caracter dollar sign and one 
-                                                    //in combination with the curly braces
+                                                 //in combination with the curly braces
     return <li classes={classes.meal}>
             <div><h3>{props.name}</h3>
             <div className={classes.description}>{props.description}</div>
             <div className = {classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm id = {props.id}/>
+                <MealItemForm id = {props.id} onAddToCart={addToCartHandler}/>
             </div>        
         </li>
 }
